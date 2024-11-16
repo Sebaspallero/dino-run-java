52f7a6b5cc8930b5515c387bf1e3bcd46908079b
Sebastian Pallero <sebastianoria@hotmail.com>
2024-11-16 19:14:32 -0300

    feature/obstacle-entity-generation

--- src/game/entities/Obstacle.java
+++ src/game/entities/Obstacle.java
@@ -1,5 +1,34 @@
 package game.entities;
+import java.awt.*;
 
 public class Obstacle {
 
+    private int x, y, width, height, speed;
+
+    private static final int GROUND_Y = 300 - 20;
+    private static final int SPAWN_X = 800;
+
+    public Obstacle(int width, int height, int speed) {
+        this.x = SPAWN_X;
+        this.y = GROUND_Y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+    }
+
+    public void update() {
+        x -= speed;
+    }
+
+    public boolean isOutOfScreen(){
+       return x + width < 0;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.BLUE);         
+        g.fillRect(x, y, width, height); 
+    }
+    
+    
+    
 }
